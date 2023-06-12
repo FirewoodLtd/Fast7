aef6a26dbf52b4e96d1ec32f24bc80c17bcde554
jack douglas <jackdouglas@jacks-MacBook-Pro-1409.local>
2023-06-12 14:36:28 +0100

    commit 2

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,19 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import * as bootstrap from "bootstrap"
+
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+//= require jquery
+//= require jquery_ujs
+//= require popper
+//= require bootstrap-sprockets
+//= require filterrific/filterrific-jquery
+//= require date_wrapper
+//= require datatables
+//= require tinymce
+//= require highcharts
+//= require highcharts/highcharts-more
+//= require highcharts/modules/funnel
+//= require highcharts/modules/annotations
+
+//= require_tree .
